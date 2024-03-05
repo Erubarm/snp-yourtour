497798bdded42dd154ef943e9122579735404592
erubarn <kirill.komigachev@yandex.ru>
2024-03-06 01:08:41 +0300

    Исправлены пункты: 1,2, 3, 5, 6, 8

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,32 @@
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 const heightShowHeader = 450; // Момент, с которого начинается фиксация
-const headerLogo = header.querySelector('img');
+const headerLogo = header.querySelector("img");
+
+const listButtonSortSection = document.querySelectorAll('.section-link');
+let lastFocused = listButtonSortSection[0];
+console.log(lastFocused);
 
 
+for ( let i = 0; i < listButtonSortSection.length; i++ ) {
+    
+    listButtonSortSection[i].addEventListener('click', () => {
+        console.log(lastFocused);
+        // console.log(listButtonSortSection[i]);
+        lastFocused.classList.remove('section-link__focus');
+        this.classList.add('section-link__focus');
+    })
 
-window.addEventListener('scroll', () => {
-    let scrollHight = window.scrollY;
+    lastFocused = this;
+}
 
-    if (scrollHight >= heightShowHeader) {
-        header.classList.add('header-fix');
-        headerLogo.src = './assets/icons/YourTour-black.svg';
-    } else {
-        header.classList.remove('header-fix');
-        headerLogo.src = './assets/icons/your-tour.svg';
-    }
-})
+window.addEventListener("scroll", () => {
+  let scrollHight = window.scrollY;
+
+  if (scrollHight >= heightShowHeader) {
+    header.classList.add("header-fix");
+    headerLogo.src = "./assets/icons/YourTour-black.svg";
+  } else {
+    header.classList.remove("header-fix");
+    headerLogo.src = "./assets/icons/your-tour.svg";
+  }
+});
